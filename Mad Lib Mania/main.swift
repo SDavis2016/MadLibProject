19cd2151953c81f3ff2be509dc78e542b5df01b5
Sebo <16sdavis@Coupeville.k12.wa.us>
2016-01-06 11:12:36 -0800

    yeppers

--- Mad Lib Mania/main.swift
+++ Mad Lib Mania/main.swift
@@ -13,4 +13,47 @@ func input() -> String {
     var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
     return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
-print("jose marcos")
+print("Welcome to Mad Lib Mania! Where you have hundreds (5) of fun (kinda boring) Mad Libs to choose from. Please, select your Mad Lib number!")
+var game = true
+while game == true {
+    print("Main Menu")
+    print("1. Someones")
+    print("2. Someone elses")
+    print("3. The other persons")
+    print("4. Whoever finishes their first one")
+    print("5. The next person")
+    print("6. Exit the Mania")
+    var select = input()
+    if select == "1" {
+        var play1 = true
+        while play1 == true {
+            
+        }
+    } else if select == "2" {
+        var play2 = true
+        while play2 == true {
+            
+        }
+    } else if select == "3" {
+        var play3 = true
+        while play3 == true {
+            
+        }
+    }else if select == "4" {
+        var play4 = true
+        while play4 == true {
+            
+        }
+    }else if select == "5" {
+        var play5 = true
+        while play5 == true {
+            
+        }
+    }else if select == "6" {
+        print("Thanks for experiencing the wasted time of your life!")
+        game = false
+    } else {
+        print("Please select a game number. If you would like to exit and save yourself press 6.")
+        game = true
+    }
+}
