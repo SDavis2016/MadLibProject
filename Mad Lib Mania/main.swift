3044c0d5721cf193c74ed6291919c6633d454ef4
Sebo <16sdavis@Coupeville.k12.wa.us>
2016-01-06 10:33:12 -0800

    Initial Commit

---
+++ Mad Lib Mania/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mad Lib Mania
+//
+//  Created by Sebastian Davis on 1/6/16.
+//  Copyright © 2016 Epic Gaming Prod. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
