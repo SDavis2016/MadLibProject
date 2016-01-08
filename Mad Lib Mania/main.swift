5896e4f240ed036d623608bed3b723dc6d9eacb0
Guest User <coder@mainlab20.local>
2016-01-08 11:03:38 -0800

    samuel

--- Mad Lib Mania/main.swift
+++ Mad Lib Mania/main.swift
@@ -13,6 +13,61 @@ func input() -> String {
     var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
     return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
-print("ballz")
+print("Welcome to Mad Lib Mania! Where you have hundreds (5) of fun (kinda boring) Mad Libs to choose from. Please, select your Mad Lib number!")
+var game = true
+while game == true {
+    print("Main Menu")
+    print("1. Samuel's write in play1 while statement")
+    print("2. Jose's")
+    print("3. Sebo's")
+    print("4. Whoever finishes their first one")
+    print("5. The next person")
+    print("6. Exit the Mania")
+    var select = input()
+    if select == "1" {
+        var play1 = true
+        while play1 == true {
+            print ("please add a noun")
+            var noun1 = input()
+            print("please add a noun")
+            var noun2 = input()
+            print("please name a place")
+            var place1 = input()
+            print("please add a noun")
+            var noun3 = input()
+            print("please add an adverb")
+            var adverb = input()
+            print("please add a noun")
+            var noun4 = input()
+        }
+    } else if select == "2" {
+        var play2 = true
+        while play2 == true {
+            
+        }
+    } else if select == "3" {
+        var play3 = true
+        while play3 == true {
+            
+        }
+    }else if select == "4" {
+        var play4 = true
+        while play4 == true {
+            
+        }
+    }else if select == "5" {
+        var play5 = true
+        while play5 == true {
+            
+        }
+    }else if select == "6" {
+        print("Thanks for experiencing the wasted time of your life!")
+        game = false
+    } else {
+        print("Please select a game number. If you would like to exit and save yourself press 6.")
+        game = true
+    }
+}
+
 
 
