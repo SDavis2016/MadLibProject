d4642969db45c8afabb7617c06b3bab12f56e316
Sebo <16sdavis@Coupeville.k12.wa.us>
2016-01-09 18:00:56 -0800

    ec sebo

--- Mad Lib Mania/main.swift
+++ Mad Lib Mania/main.swift
@@ -13,6 +13,7 @@ func input() -> String {
     var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
     return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
+var savedLibs: [String] = []
 print("Welcome to Mad Lib Mania! Where you have hundreds (5) of fun (kinda boring) Mad Libs to choose from. Please, select your Mad Lib number!")
 var game = true
 while game == true {
@@ -22,7 +23,8 @@ while game == true {
     print("3. Lunch Room")
     print("4. PLease Name Your Mad Lib")
     print("5. PLease Name Your Mad Lib")
-    print("6. Exit the Mania")
+    print("6. Review an already completed Mad Lib")
+    print("7. Exit the Mania")
     var select = input()
     if select == "1" {
         var play1 = true
@@ -100,6 +102,8 @@ while game == true {
             print("Your completed Mad Lib is:")
             print("Make sure your lunch \(container3) is filled with nutritious \(adj3) food. Do not go to the \(adj32) food stand across the street from school. The hamburgers they serve are fried in \(noun3) and are made of \(ani3) meat. So take a sandwich made of \(veggie3) or \(veggie32) it's much healthier! Drink \(col3) milk instead of \(adj33) colas.")
             print("")
+            var lib3 = "Make sure your lunch \(container3) is filled with nutritious \(adj3) food. Do not go to the \(adj32) food stand across the street from school. The hamburgers they serve are fried in \(noun3) and are made of \(ani3) meat. So take a sandwich made of \(veggie3) or \(veggie32) it's much healthier! Drink \(col3) milk instead of \(adj33) colas."
+            savedLibs.append(lib3)
             print("Would you like to play again? If so, press 1. If you would like to return to the main menu, press 2. If you are done with Mad Lib Mania, to exit press 3.")
             var again3 = true
             while again3 == true {
@@ -132,10 +136,37 @@ while game == true {
             
         }
     }else if select == "6" {
+        var review = true
+        while review == true {
+        print("Which Mad Lib would you like to review?")
+        print("1. ")
+        print("2. ")
+        print("3. Lunch Room")
+        print("4. ")
+        print("5. ")
+        print("6. Return to main menu")
+        var reviewSelect = input()
+            if reviewSelect == "1" {
+                print(savedLibs[0])
+                
+            }else if reviewSelect == "2" {
+                
+            }else if reviewSelect == "3" {
+                
+            }else if reviewSelect == "4" {
+                
+            }else if reviewSelect == "5" {
+                
+            } else if reviewSelect == "6" {
+                review = false
+                game = true
+            }
+        }
+    }else if select == "7" {
         print("Thanks for experiencing the wasted time of your life!")
         game = false
     } else {
-        print("Please select a game number. If you would like to exit and save yourself press 6.")
+        print("Please select a game number. If you would like to exit and save yourself press 7.")
         game = true
     }
 }
