471175f0cac1fa0fae35bb8acdb3b3c23268804c
Guest User <coder@mainlab29.local>
2016-01-12 11:17:05 -0800

    I love you

--- Mad Lib Mania/main.swift
+++ Mad Lib Mania/main.swift
@@ -147,7 +147,37 @@ while game == true {
     }else if select == "4" {
         var play4 = true
         while play4 == true {
-            
+            print("Type a Verd ending in -ing")
+            var verd4 = input()
+            print("Type an animal")
+            var animal4 = input()
+            print("Type an adjective")
+            var adjective4 = input()
+            print("Type a place")
+            var place4 = input()
+            print("Type a verd")
+            var verd44 = input()
+            print("There are to many \(verd4) \(animal4) on this \(adjective4) \(place4)! He screamed while he \(verd44)")
+            print("Would you like to play again? (warning it will reset all your previous answers)  If so, press 1. If you would like to return to the main menu, press 2. If you are done with Mad Lib Mania, to exit press 3.")
+            var again3 = true
+            while again3 == true {
+                var finished3 = input()
+                if finished3 == "1" {
+                    play4 = true
+                    again3 = false
+                } else if finished3 == "2" {
+                    again3 = false
+                    play4 = false
+                    game = true
+                } else if finished3 == "3" {
+                    again3 = false
+                    play4 = false
+                    game = false
+                } else {
+                    print("Please select your next step! 1 lets you play again, 2 takes you back to main menu, 3 exits Mad Lib Mania!")
+                    again3 = true
+                }
+            }
         }
     }else if select == "5" {
         var play5 = true
