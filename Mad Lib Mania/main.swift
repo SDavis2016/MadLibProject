4bf725febbd7c5a793a0793cf9a649c22b4a6fec
Guest User <coder@mainlab20.local>
2016-01-14 11:12:03 -0800

    sam

--- Mad Lib Mania/main.swift
+++ Mad Lib Mania/main.swift
@@ -29,7 +29,7 @@ while game == true {
     if select == "1" {
         var play1 = true
         while play1 == true {
-// i samuel bishop did the prints and var statements you see here.
+// i samuel bishop did the prints and var statements you see here. It alows you to play the first mad lib.
             print ("please add a noun")
             var noun1 = input()
             print("please add a noun")
@@ -214,7 +214,7 @@ this is when your done with the mad lib it will give you the option to do anothe
                 }
             }
         }
-// i samuel bishop did the print, var and else if statments you see here.
+// i samuel bishop did the print, var and else if statments you see here. It inables you to play the fifth mad lib.
     }else if select == "5" {
         var play5 = true
         while play5 == true {
